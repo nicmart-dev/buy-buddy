2666d94671a0a6887e944d0f5dfd2598488afa59
Grace Lau <grace@gracelau.ca>
2024-05-16 12:33:14 -0700

    deleted accidental sass install on main parent folder

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,20 @@ function App() {
       </header>
 
       <div className ="list__container">
-        <h1 className ="list__wrapper">Welcme to BuyBuddy</h1>
+        <h1 className ="list__title">Welcome to BuyBuddy</h1>
+
+        <h2 className="list__subtitle">all shopping items</h2>
+        <ul className="list__items">
+          <li>Placeholder</li>
+          <li>Placeholder</li>
+          <li>Placeholder</li>
+          <li>Placeholder</li>
+          <li>Placeholder</li>
+         
+        </ul>
+
+        <button className="list__button">add shopping item</button>
+
 
       </div>
     </div>
