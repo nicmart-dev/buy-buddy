8ecfea397cbf41a5d73105cf9d8201314f65389f
Nicolas Martinez <nicmart@gmail.com>
2024-05-16 14:32:23 -0700

    merge page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import axios from "axios";
-import './App.scss';
+import "./App.scss";
+import AddItem from "./Pages/AddItem";
 
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
 
+  const url = "http://localhost:5050";
 
-const url = "http://localhost:5050";
+  useEffect(() => {
+    const getShoppingList = async () => {
+      const response = await axios.get(`${url}/shopping-list`);
+      setShoppingList(response.data);
+    };
+    getShoppingList();
+  }, [shoppingList]);
 
-
-
-useEffect(() => {
-const getShoppingList = async () => {
-
-  const response = await axios.get(
-    `${url}/shopping-list`
-  );
-  setShoppingList(response.data);
-};
-getShoppingList();
-}, []);
-
-
-
-
-  
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
 
-      <div className ="list__container">
-        <h1 className ="list__title">Welcome to BuyBuddy</h1>
+      <div className="list__container">
+        <h1 className="list__title">Welcome to BuyBuddy</h1>
 
         <h2 className="list__subtitle">all shopping items</h2>
         <ul className="list__items">
-        {shoppingList
-        .map((item) => (
-          <li key={item.id}>
-          <h5> {item.quantity} x {item.name} </h5>
-         
-          </li>
-        ))}
+          {shoppingList.map((item) => (
+            <li key={item.id}>
+              <h5>
+                {" "}
+                {item.quantity} x {item.name}{" "}
+              </h5>
+            </li>
+          ))}
         </ul>
-
-        <button className="list__button">add shopping item</button>
-
-
+        <AddItem />
       </div>
     </div>
   );
